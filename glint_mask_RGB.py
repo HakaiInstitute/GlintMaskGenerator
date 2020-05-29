4598a9ed46be52d0c32f36d64c4df1d658253c86
Taylor Denouden <taylordenouden@gmail.com>
2020-05-29 12:52:43 -0400

    Tif support. Separate out logic from file saving. Improve progress bars

--- glint_mask_RGB.py
+++ glint_mask_RGB.py
@@ -4,7 +4,7 @@
 # Based on Matlab script by Tom Bell written 6/28/2019
 #
 # Description: Generate masks for glint regions in in RGB Images.
-
+import itertools
 from pathlib import Path
 
 import numpy as np
@@ -16,7 +16,7 @@ from tqdm import tqdm
 Image.MAX_IMAGE_PIXELS = None
 
 
-def make_mask(img_path, mask_out_path, glint_threshold=0.9, mask_buffer_sigma=20, num_bins=8):
+def make_and_save_mask(img_path, mask_out_path, glint_threshold=0.9, mask_buffer_sigma=20, num_bins=8):
     """
     Create and return a glint mask for RGB imagery.
 
@@ -30,8 +30,7 @@ def make_mask(img_path, mask_out_path, glint_threshold=0.9, mask_buffer_sigma=20
         Play with this value. Default is 0.9.
     :param mask_buffer_sigma: The sigma for the Gaussian kernel used to buffer the mask. Defaults to 20
     :param num_bins: The number of bins the blue channel is slotted into. Defaults to 8 as in Tom's script.
-    :return: Numpy array of mask for results on single files, None for directory processing.
-        Side effects are that the mask is saved to the specified mask_out_path location.
+    :return: None. Side effects are that the mask is saved to the specified mask_out_path location.
     """
     if Path(img_path).is_dir():
         if not Path(mask_out_path).is_dir():
@@ -39,42 +38,63 @@ def make_mask(img_path, mask_out_path, glint_threshold=0.9, mask_buffer_sigma=20
                 "img_path and mask_out_path must both be a path to a directory, or both be a path to a named file.")
 
         # Get all images in the specified directory
-        for ext in tqdm(("png", "PNG", "jpg", "JPG", "jpeg", "JPEG")):
-            for path in tqdm(list(Path(img_path).glob(f"*.{ext}"))):
-                out_path = Path(mask_out_path).joinpath(f"{path.stem}_mask.png")
-                _ = make_mask(str(path), str(out_path),
-                              glint_threshold=glint_threshold, mask_buffer_sigma=mask_buffer_sigma, num_bins=num_bins)
-
+        extensions = ("png", "PNG", "jpg", "JPG", "jpeg", "JPEG", "tif", "TIF")
+        img_paths = itertools.chain.from_iterable((Path(img_path).glob(f"*.{ext}") for ext in extensions))
     else:
-        # Open the image
-        img = Image.open(img_path)
+        img_paths = [Path(img_path)]
 
-        # Use RGB bands only
-        img = np.array(img)[:, :, :3]
-
-        # Get the Blue Channel
-        blue = img[:, :, 2]
-
-        # Quantize blue channel into num_bins bins
-        bins = np.linspace(0, 255, num_bins, endpoint=False)
-        si = np.digitize(blue, bins)
-        # Rescale to (0, 1) range
-        sis = (si.astype(np.float) - si.min()) / (si.max() - si.min())
-
-        # Find Glint Threshold and Set those Pixels to 1
-        mask = (sis <= glint_threshold)
-
-        # Create Buffered Mask
-        mask_buffered = gaussian_filter(mask.astype(np.float), mask_buffer_sigma)
-        mask = mask_buffered >= 0.99
-
+    for path in tqdm(img_paths):
+        out_path = Path(mask_out_path).joinpath(f"{path.stem}_mask.png")
+        mask = make_single_mask(str(path), glint_threshold=glint_threshold,
+                                mask_buffer_sigma=mask_buffer_sigma, num_bins=num_bins)
         # Save the mask
-        mask = mask.astype(np.uint8) * 255
         mask_img = Image.fromarray(mask, mode='L')
-        mask_img.save(mask_out_path)
+        mask_img.save(out_path)
 
-        return mask
+
+def make_single_mask(img_path, glint_threshold=0.9, mask_buffer_sigma=20, num_bins=8):
+    """
+    Create and return a glint mask for RGB imagery.
+
+    :param img_path: The path to a named input image or directory containing images.
+        Supported formats for single image processing are here:
+        https://pillow.readthedocs.io/en/stable/handbook/image-file-formats.html.
+        If img_path is a directory, all jpg, jpeg, and png images in that directory will be processed.
+    :param mask_out_path: The path to send your out image including the file name and type. e.g. "/path/to/mask.png".
+        mask_out_path must be a directory if img_path is specified as a directory.
+    :param glint_threshold: The amount of binned "blueness" that should be glint. Domain for values is (0.0, 1.0).
+        Play with this value. Default is 0.9.
+    :param mask_buffer_sigma: The sigma for the Gaussian kernel used to buffer the mask. Defaults to 20
+    :param num_bins: The number of bins the blue channel is slotted into. Defaults to 8 as in Tom's script.
+    :return: Numpy array of mask.
+    """
+    # Open the image
+    img = Image.open(img_path)
+
+    # Use RGB bands only
+    img = np.array(img)[:, :, :3]
+
+    # Get the Blue Channel
+    blue = img[:, :, 2]
+
+    # Quantize blue channel into num_bins bins
+    bins = np.linspace(0, 255, num_bins, endpoint=False)
+    si = np.digitize(blue, bins)
+    # Rescale to (0, 1) range
+    sis = (si.astype(np.float) - si.min()) / (si.max() - si.min())
+
+    # Find Glint Threshold and Set those Pixels to 1
+    mask = (sis <= glint_threshold)
+
+    # Create Buffered Mask
+    mask_buffered = gaussian_filter(mask.astype(np.float), mask_buffer_sigma)
+    mask = mask_buffered >= 0.99
+
+    # Save the mask
+    mask = mask.astype(np.uint8) * 255
+
+    return mask
 
 
 if __name__ == '__main__':
-    Fire(make_mask)
+    Fire(make_and_save_mask)
--- specular_mask.py
+++ specular_mask.py
@@ -5,6 +5,7 @@
 # Description: Generate masks for glint regions in in RGB Images.
 
 
+import itertools
 import math
 from pathlib import Path
 
@@ -53,7 +54,7 @@ def estimate_specular_reflection_component(img, percent_diffuse):
     return spec_ref_est
 
 
-def make_mask(img_path, mask_out_path, percent_diffuse=0.1, mask_thresh=0.2, opening_iterations=0):
+def make_and_save_mask(img_path, mask_out_path, percent_diffuse=0.1, mask_thresh=0.2, opening_iterations=0):
     """
     Create and return a glint mask for RGB imagery.
 
@@ -69,8 +70,7 @@ def make_mask(img_path, mask_out_path, percent_diffuse=0.1, mask_thresh=0.2, ope
         E.g. if more than 50% specular reflectance is unacceptable, use 0.5. Default is 0.2.
     :param opening_iterations: The number of morphological opening iterations on the produced mask.
         Useful for closing small holes in the mask. Set to 0 by default (i.e. it's shut off).
-    :return: Numpy array of mask for results on single files, None for directory processing.
-        Side effects are that the mask is saved to the specified mask_out_path location.
+    :return: None. Side effects are that the mask is saved to the specified mask_out_path location.
     """
     if Path(img_path).is_dir():
         if not Path(mask_out_path).is_dir():
@@ -78,36 +78,56 @@ def make_mask(img_path, mask_out_path, percent_diffuse=0.1, mask_thresh=0.2, ope
                 "img_path and mask_out_path must both be a path to a directory, or both be a path to a named file.")
 
         # Get all images in the specified directory
-        for ext in tqdm(("png", "PNG", "jpg", "JPG", "jpeg", "JPEG")):
-            for path in tqdm(list(Path(img_path).glob(f"*.{ext}"))):
-                out_path = Path(mask_out_path).joinpath(f"{path.stem}_mask.png")
-                _ = make_mask(str(path), str(out_path),
-                              percent_diffuse=percent_diffuse, mask_thresh=mask_thresh,
-                              opening_iterations=opening_iterations)
-
+        extensions = ("png", "PNG", "jpg", "JPG", "jpeg", "JPEG", "tif", "TIF")
+        img_paths = itertools.chain.from_iterable((Path(img_path).glob(f"*.{ext}") for ext in extensions))
     else:
-        # Open the image
-        img = Image.open(img_path)
+        img_paths = [Path(img_path)]
 
-        # Use RGB bands only
-        img = np.array(img)[:, :, :3] / 255.
-
-        spec_ref = estimate_specular_reflection_component(img, percent_diffuse)
-
-        # Generate the mask
-        mask = (spec_ref <= mask_thresh)
-
-        # Fill in small holes in the mask
-        if opening_iterations > 0:
-            mask = binary_opening(mask.astype(np.uint8), iterations=opening_iterations).astype(np.bool)
-
+    for path in tqdm(img_paths):
+        out_path = Path(mask_out_path).joinpath(f"{path.stem}_mask.png")
+        mask = make_single_mask(str(path), percent_diffuse=percent_diffuse, mask_thresh=mask_thresh,
+                                opening_iterations=opening_iterations)
         # Save the mask
-        mask = mask.astype(np.uint8) * 255
         mask_img = Image.fromarray(mask, mode='L')
-        mask_img.save(mask_out_path)
+        mask_img.save(out_path)
 
-        return mask
+
+def make_single_mask(img_path, percent_diffuse=0.1, mask_thresh=0.2, opening_iterations=0):
+    """
+    Create and return a glint mask for RGB imagery.
+
+    :param img_path: The path to a named input image or directory containing images.
+        Supported formats for single image processing are here:
+        https://pillow.readthedocs.io/en/stable/handbook/image-file-formats.html.
+        If img_path is a directory, all jpg, jpeg, and png images in that directory will be processed.
+    :param percent_diffuse: An estimate of the percentage of pixels in an image that show pure diffuse reflectance, and
+        thus no specular reflectance (glint). Defaults to 0.1. Try playing with values, low ones typically work well.
+    :param mask_thresh: The threshold on the specular reflectance estimate image to convert into a mask.
+        E.g. if more than 50% specular reflectance is unacceptable, use 0.5. Default is 0.2.
+    :param opening_iterations: The number of morphological opening iterations on the produced mask.
+        Useful for closing small holes in the mask. Set to 0 by default (i.e. it's shut off).
+    :return: Numpy array of mask.
+    """
+    # Open the image
+    img = Image.open(img_path)
+
+    # Use RGB bands only
+    img = np.array(img)[:, :, :3] / 255.
+
+    spec_ref = estimate_specular_reflection_component(img, percent_diffuse)
+
+    # Generate the mask
+    mask = (spec_ref <= mask_thresh)
+
+    # Fill in small holes in the mask
+    if opening_iterations > 0:
+        mask = binary_opening(mask.astype(np.uint8), iterations=opening_iterations).astype(np.bool)
+
+    # Save the mask
+    mask = mask.astype(np.uint8) * 255
+
+    return mask
 
 
 if __name__ == '__main__':
-    Fire(make_mask)
+    Fire(make_and_save_mask)
