d93dcb902de39fefa74f20a17af4004877607cf0
Taylor Denouden <taylordenouden@gmail.com>
2020-06-03 16:16:42 -0400

    Fix test assumptions

--- test/test_common.py
+++ test/test_common.py
@@ -6,8 +6,14 @@ from core.common import get_img_paths
 IMG_CONTENT = Image.fromarray(np.ones((32, 32, 3)).astype(np.uint8) * 255)
 
 
+def _clean_dir(dir_path):
+    for f in dir_path.glob('*'):
+        f.unlink()
+
+
 def test_get_img_paths(tmp_path):
     # Test detection of RGB files
+    _clean_dir(tmp_path)
     true_paths = ["1.jpg", "2.JPG", "3.tif", "4.tiff", "5.TIF", "6.TIFF", "7.png", "8.PNG", "9.jpeg", "10.JPEG"]
     true_paths = sorted([str(tmp_path.joinpath(n)) for n in true_paths])
     print(true_paths)
@@ -20,6 +26,7 @@ def test_get_img_paths(tmp_path):
     assert all(np.array(true_paths) == np.array(img_paths))
 
     # Test detection of RedEdge files
+    _clean_dir(tmp_path)
     true_paths = [
         "IMG_1234_1.tif", "IMG_1234_2.tif", "IMG_1234_3.tif", "IMG_1234_4.tif", "IMG_1234_5.tif",
         "IMG_4321_1.TIF", "IMG_4321_2.TIF", "IMG_4321_3.TIF", "IMG_4321_4.TIF", "IMG_4321_5.TIF",
