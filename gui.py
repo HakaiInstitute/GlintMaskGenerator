49a43ca562b376d0178099a09ee55c9ca0eaf696
Taylor Denouden <taylordenouden@gmail.com>
2020-06-30 12:51:58 -0400

    Reduce default number of workers in GUI

--- gui.py
+++ gui.py
@@ -43,7 +43,7 @@ class GlintMaskApp(ttk.Frame):
     def __init__(self, master, *args, **kwargs):
         super().__init__(master, *args, **kwargs)
         self.LABEL_WIDTH = 16
-        self.DEFAULT_WORKERS = os.cpu_count() * 5
+        self.DEFAULT_WORKERS = 5
         self.IMG_TYPES = {
             # Map display names to class required to process the images
             'RGB / CIR (specular method)': RGBSpecularMasker,
